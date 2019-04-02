5f95d9ddeace3254aa78ad36d79f831ae27882ed
EsmiraldaZ <32571297+EsmiraldaZ@users.noreply.github.com>
2019-04-02 19:13:45 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,21 +1,31 @@
-﻿
-function quicksort(array) {
-   if (array.length <= 1) {
-	   return array;
-  }
-
-  var opora = array[0];
-  var left = []; 
-  var right = [];
-
-  for (var i = 1; i < array.length; i++) {
-    array[i] < opora ? left.push(array[i]) : right.push(array[i]);
-  }
-
-  return quicksort(left).concat(opora, quicksort(right));
-};
-
-var myMassiv = [77, 18, 85, 8, 33, 97, 22];
-var sorted = quicksort(myMassiv);
-
-console.log('Sorted array', sorted);
+"user strict";
+arr = [4, 15, -7, 5, 3, 6, -5, 6, 0, 2, 1];
+function quickSort(arr, left, right) {
+   var mid = arr[Math.floor((right + left) / 2)];
+   var i = left;
+   var j = right;
+   while (i < j) {
+      while (arr[i] < mid) {
+         i++;
+      }
+      while (arr[j] > mid) {
+         j--;
+      }
+      if (i <= j) {
+         var tmp = arr[i];
+         arr[i] = arr[j];
+         arr[j] = tmp;
+         i++;
+         j--;
+      }
+   }
+   if (left < j) {
+      quickSort(arr, left, j);
+   }
+   if (i < right) {
+      quickSort(arr, i, right);
+   }
+   return arr;
+}
+var result = quickSort(arr, 0, arr.length-1);
+console.log(result);
